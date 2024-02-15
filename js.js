13a609f1f851798f81724d09f08ba75e3c98f4bd
Zed-jaanko <153653497+Zed-jaanko@users.noreply.github.com>
2024-02-15 20:44:25 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
 function Zed(){
     let zed =new URL("https://www.w3schools.com/css/css_units.asp");
-    console.log(zed.pathname)
+    console.log(zed.pathname);
+    console.log(pathname);
+    alert("Zed jamshed jaanko");
 }
